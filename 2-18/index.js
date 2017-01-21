0becc370c9e2baf1e8a211f6902f5390f0d0420d
sjingye <sjingye@163.com>
2017-01-21 20:36:55 +0800

    提交2-18

--- 2-18/index.js
+++ 2-18/index.js
@@ -1,12 +1,13 @@
 //DOM
 var inp = document.getElementById("inp");
-var btns = document.getElementsByTagName("button");
+var btns = document.querySelectorAll(".btn");
 var lin = btns[0];
 var rin = btns[1];
 var lout = btns[2];
 var rout = btns[3];
-var wrap = document.getElementsByClassName("wrap")[0];
+var wrap = document.querySelector(".wrap");
 var arr = [];
+
 //通用的一些方法
 function addEvent(element,event,listener) {
     if(element.addEventListener){
@@ -18,27 +19,54 @@ function addEvent(element,event,listener) {
     else{
         element["on"+event] = listener;
     }
-}
+};
+
+//为各个按钮加上点击事件
 addEvent(lin,"click",function () {
-    if( !(/^\d+$/).test(inp.value.trim()) ){
-        alert("请输入正整数");
+    var val = inp.value.trim();
+    if( !(/^(?:[1-9]\d|100)$/g).test(val) ){
+        alert("请输入10-100之间的正整数");
     }
     else{
-        console.log(1);
+        arr.unshift( val );
+    }
+    draw();
+    inp.value="";
+});
+addEvent(rin,"click",function () {
+    var val = inp.value.trim();
+    if( !(/^(?:[1-9]\d|100)$/g).test(val) ){
+        alert("请输入10-100之间的正整数");
+    }
+    else{
+        arr.push( getValue() );
+    }
+    draw();
+    inp.value="";
+});
+
+addEvent(lout,"click",function () {
+    alert(arr.shift());
+    draw();
+});
+
+addEvent(rout,"click",function () {
+    alert(arr.pop());
+    draw();
+});
+
+//为每个加上的元素添加点击事件
+addEvent(wrap,"click",function (e) {
+    if(e.target.nodeName === "LI"){
+        var index = arr.indexOf(e.target.innerHTML);
+        arr.splice(index,1);
+        draw();
     }
 });
-/*btns[0].addEventListener("click",function () {
-
-    arr.unshift(inp.value);
-
-    // draw();
-});*/
+//根据数组修改wrap的html
 function draw() {
-    wrap.innerHTML="";
-    var str = "";
-    arr.forEach(function (value,index) {
-        str += "<li>"+value+"</li>";
-    });
-    wrap.innerHTML=str;
+    wrap.innerHTML= arr.map(function (item) {
+        return "<li>"+item+"</li>";
+    }).join("");
 };
 
