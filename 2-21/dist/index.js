b617166328c87bacf408aad93fb3099a2927af93
sjingye <sjingye@163.com>
2017-02-06 20:21:07 +0800

    2-29

--- 2-21/dist/index.js
+++ 2-21/dist/index.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -71,9 +71,46 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+const EventUtil = {
+    addEvent: (element, event, listener) => {
+        //验证方法是否存在
+        if (element.addEventListener) {
+            element.addEventListener(event, listener);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + event, listener);
+        } else {
+            element["on" + event] = listener;
+        }
+    },
+    getEvent: event => event ? event : window.event,
+    getTarget: event => event.target ? event.target : event.srcElement
+};
+/* harmony default export */ __webpack_exports__["a"] = EventUtil;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const RenderLIst = {
+    render: (element, list) => {
+        let html = "";
+        list.map(function (item) {
+            html += `<li>${item}</li>`;
+        });
+        element.innerHTML = html;
+    }
+};
+/* harmony default export */ __webpack_exports__["a"] = RenderLIst;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__event_js__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__render_js__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__event_js__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__render_js__ = __webpack_require__(1);
 
 
 
@@ -185,43 +222,6 @@ class hobbyEle {
 }
 let b = new hobbyEle(hobbyContainer);
 b.init();
-
-/***/ }),
-/* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-const EventUtil = {
-    addEvent: (element, event, listener) => {
-        //验证方法是否存在
-        if (element.addEventListener) {
-            element.addEventListener(event, listener);
-        } else if (element.attachEvent) {
-            element.attachEvent("on" + event, listener);
-        } else {
-            element["on" + event] = listener;
-        }
-    },
-    getEvent: event => event ? event : window.event,
-    getTarget: event => event.target ? event.target : event.srcElement
-};
-/* harmony default export */ __webpack_exports__["a"] = EventUtil;
-
-/***/ }),
-/* 2 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-const RenderLIst = {
-    render: (element, list) => {
-        let html = "";
-        list.map(function (item) {
-            html += `<li>${item}</li>`;
-        });
-        element.innerHTML = html;
-    }
-};
-/* harmony default export */ __webpack_exports__["a"] = RenderLIst;
 
 /***/ })
 /******/ ]);
